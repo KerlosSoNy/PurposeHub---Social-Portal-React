64db57a5574c4e06a21c38b349e7af351dcedb0f
Kerlos Magdy <kerlosssony@gmail.com>
2025-01-06 01:50:16 +0200

    Showing Some Posts To Test

--- src/components/midComponent/midComponent.tsx
+++ src/components/midComponent/midComponent.tsx
@@ -20,6 +20,50 @@ export default function MidComponent() {
                 comments={12}
                 userAvatarUrl="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=100&h=100&fit=crop"
             />
+            <Post
+                avatarUrl="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=100&h=100&fit=crop"
+                username="Ahmed Sayed"
+                timestamp="16h"
+                content="This 3 shows are special🤝  Part (1/7)"
+                hashtags={['#TheOwlHouse', '#DemonSlayer', '#AvatarTheLastAirbender']}
+                imageUrl="https://images.unsplash.com/photo-1542831371-29b0f74f9713?w=600&h=400&fit=crop"
+                likes={123}
+                comments={12}
+                userAvatarUrl="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=100&h=100&fit=crop"
+            />
+            <Post
+                avatarUrl="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=100&h=100&fit=crop"
+                username="Ahmed Sayed"
+                timestamp="16h"
+                content="This 3 shows are special🤝  Part (1/7)"
+                hashtags={['#TheOwlHouse', '#DemonSlayer', '#AvatarTheLastAirbender']}
+                imageUrl="https://images.unsplash.com/photo-1542831371-29b0f74f9713?w=600&h=400&fit=crop"
+                likes={123}
+                comments={12}
+                userAvatarUrl="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=100&h=100&fit=crop"
+            />
+            <Post
+                avatarUrl="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=100&h=100&fit=crop"
+                username="Ahmed Sayed"
+                timestamp="16h"
+                content="This 3 shows are special🤝  Part (1/7)"
+                hashtags={['#TheOwlHouse', '#DemonSlayer', '#AvatarTheLastAirbender']}
+                imageUrl="https://images.unsplash.com/photo-1542831371-29b0f74f9713?w=600&h=400&fit=crop"
+                likes={123}
+                comments={12}
+                userAvatarUrl="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=100&h=100&fit=crop"
+            />
+            <Post
+                avatarUrl="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=100&h=100&fit=crop"
+                username="Ahmed Sayed"
+                timestamp="16h"
+                content="This 3 shows are special🤝  Part (1/7)"
+                hashtags={['#TheOwlHouse', '#DemonSlayer', '#AvatarTheLastAirbender']}
+                imageUrl="https://images.unsplash.com/photo-1542831371-29b0f74f9713?w=600&h=400&fit=crop"
+                likes={123}
+                comments={12}
+                userAvatarUrl="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=100&h=100&fit=crop"
+            />
         </div>
     )
 }
